7b54dc3fc40af5c5fdfb5d3b6d80fec546085ebc
Elwood Nelson <elwoodnelson@gmail.com>
2024-08-29 13:17:32 -0500

    get items route is working

--- src/components/ShelfPage/ShelfPage.jsx
+++ src/components/ShelfPage/ShelfPage.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import useReduxStore from '../../hooks/useReduxStore';
+import { useDispatch } from 'react-redux';
 
 function ShelfPage() {
+  const dispatch = useDispatch()
+  const store = useReduxStore()
+
+useEffect (() => {
+    dispatch({ type: 'GET_ITEMS'})
+},[]);
+
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
+      <ul>
+        {store.shelfItems.map((shelfItems) => {
+          return (
+            <li key={shelfItems.id}>{shelfItems.description}<img className="itemImage" src={shelfItems.image_url}></img></li> 
+          )
+        })}
+      </ul>
     </div>
   );
 }
 
 export default ShelfPage;
+
